13fcf998e9e0f16beb19de92af165c7300f885f6
tjperusek <46688764+tjperusek@users.noreply.github.com>
2022-04-12 19:57:46 -0500

    good work

--- solcast_Historical_logic_trial.m
+++ solcast_Historical_logic_trial.m
@@ -1,5 +1,6 @@
 history = table2array(GetRooftopSiteEstimatedActuals);
 pv=history(:,1);
+pv = double(pv);
 dateHist = history(:,2);
 datetime1 = datetime(dateHist,'TimeZone','UTC','InputFormat','yyyy-MM-dd''T''HH:mm:ssZ');
 blank = isnat(datetime1);
@@ -28,15 +29,14 @@ dv = 0/24:1/48:23/24+2/48;
 yyaxis right
 %plot(dv,prices);
 
-sum = 0;
 devices = [150; 450; 1400; 1500; 4500; 5000];
 devices = devices/2000;
 dhours = [16; 2; 2; 2; 2; 2];
 array = zeros(49,9);
-pv = double(pv);
-
+array(:,1) = 1:49;
+array(:,2) = double(pv(1:49,1))';
 for i=1:49
-    array(i,1) = i;
+    %array(i,1) = i;
     if (prices(i) == 8.67 && pv(i) < 0.5)
         %array(i,1) = 'grid';
         array(i,3) = 1;
@@ -51,39 +51,73 @@ for i=1:49
         array(i,3) = 0;
     end
 
-    array(i,2) = pv(i);
 end
 
-sum = 0;
+check = zeros(1,6);
+j = 1;
 for i=1:49
-
-    if (array(i,2) == 2)
-        for k=1:6 %each device
-            count = dhours(k); %length of running device
-    if (array(i,3) == 2)
-        for k=1:6
-            count = dhours(k);
-
-            for j=1:dhours(k)
-                if (pv(i) > devices(k))
-                    if (count >= 0 && array(i+j-1,3) == 2 && sum < pv(i))
-                        array(i+j-1,k+3) = devices(k);
-                    end
-                    count = count - 1;
-                else
-                    array(i+j-1,k+3) = 0;
-                    count2 = 0;
-                    array(count2+j,k+3) = devices(k);
-                end
-                count2 = count2 + 1;
+    % Device counter
+    while (j < 7)
+    %for k=1:dhours(j)
+        % Reset sum when going to next device
+        sum = 0;
+        for m=1:dhours(j)
+            % Check to see if the device can be ran for entire running time
+            % during solar
+            for q=1:j
+                sum = sum + array(i,q+3);
+            end
+            sum = sum + devices(j);
+            if (array(i+m-1,2) >= sum && array(i+m-1,2) >= devices(j))
+                check(j) = 1;
+            else
+                check(j) = 0;
+                break;
             end
         end
-        break;
-    end
-    if (array(i,3) == 0)
-        array(i,4) = 0;
-    end
-    if (array(i,3) == 3)
-        array(i,4) = 0;
+        % Counter for the hours the device should be run while summing
+        sum_count = 0;
+        for k=1:dhours(j)
+            % If the recommendation is solar and the device can be ran for
+            % the entire running time
+            if (array(i+k-1,3) == 2 && check(j) == 1)
+                % If the solar generation is greater than the sum of the
+                % current running devices at that half hour, run the next
+                % device as well
+                if (array(i+k-1,2) >= sum)
+                    array(i+k-1,j+3) = devices(j);
+                % Run the device after the previous device is finished
+                else
+                    if (array(i+k-1+dhours(j-1),3) == 2)
+                        array(i+k-1,j+3) = devices(j);
+                    end
+                end
+            else
+                check(j) = 0;
+            end
+        end
+        
+        time_left = 0;
+        for n=1:49
+            % Searching for when in the array the recommendation is
+            % grid. Subtract by 1 because n starts at index 1
+            if (array(n,3) == 1 && check(j) == 0)
+                array(n,j+3) = devices(j);
+                time_left = time_left + 1;
+            end
+            if (time_left == dhours(j))
+                time_left = 0;
+                break;
+            end
+        end
+
+        if (array(i,3) == 0)
+            array(i,j+3) = 0;
+        end
+        if (array(i,3) == 3)
+            array(i,j+3) = 0;
+        end
+        % Increment which device we are placing in the array
+        j = j + 1;
     end
 end
